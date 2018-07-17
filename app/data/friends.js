453ba5684f2e39a8268751b95e129ba9d3033e2b
unknown <matthewhknetsch@gmail.com>
2018-07-17 15:10:26 -0600

    mass commit (oops)

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friends = [
+    {
+      name: 'Don Rickles',
+      photo: 'https://static01.nyt.com/images/2017/04/07/arts/07rickles-obit-4-1491512344294/07rickles-obit-4-1491512344294-blog427.jpg',
+      scores: [
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+      ]
+    },
+    {
+      name: 'Jeff Golblum',
+      photo: 'https://pmcdeadline2.files.wordpress.com/2017/04/rexfeatures_5886121s.jpg',
+      scores: [
+        '2',
+        '3',
+        '1',
+        '2',
+        '2',
+        '1',
+        '2',
+        '2',
+        '1',
+        '2'
+      ]
+    },
+    {
+      name: 'Tina Turner',
+      photo: 'http://images2.fanpop.com/images/photos/3000000/Tina-Turner-tina-turner-3033080-412-480.jpg',
+      scores: [
+        '4',
+        '4',
+        '4',
+        '2',
+        '3',
+        '2',
+        '1',
+        '1',
+        '1',
+        '1'
+      ]
+    },
+    {
+      name: 'Bill Murray',
+      photo: 'http://sheamagazine.com/wp-content/uploads/2013/09/bill-murray.jpg',
+      scores: [
+        '3',
+        '4',
+        '3',
+        '5',
+        '5',
+        '5',
+        '5',
+        '2',
+        '3',
+        '4'
+      ]
+    },
+    {
+      name: 'Hunter S. Thompson',
+      photo: 'http://www.shockmansion.com/wp-content/myimages/2017/07/Hunter-S-Thompson.jpg',
+      scores: [
+        '5',
+        '5',
+        '5',
+        '6',
+        '6',
+        '6',
+        '4',
+        '4',
+        '4',
+        '4'
+      ]
+    }
+  ];
+  module.exports = friends;
